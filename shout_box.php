2cc89e2bb3da898547399e3bc2c516d2401c786c
Šimun Strukan <simun.strukan@gmail.com>
2011-10-14 14:43:22 +0200

    shout box form

---
+++ shout_box.php
@@ -0,0 +1,39 @@
+<?  ShoutBox::setConnection($db); 
+
+	if($_POST['submit_text']) {
+		$data = array( 'name' => $_POST['nick'],
+		               'text' => $_POST['comment_text'],
+		               'date' => date("d.m.Y, H:i ") );
+		ShoutBox::insertPost($data);
+	}
+	
+	if ($action == 'delete') {
+		ShoutBox::deleteOne($id);
+		header('Location:index.php');
+
+	}
+?>
+<div id="shout_box">
+	<h2>Comments</h2>
+	<div id="shout_box_content">
+		<div id="output">
+		<? foreach(ShoutBox::getSorted() as $comment): ?>
+			<ul>
+				<li>
+					<em>On <?=$comment->date()?>, <?=$comment->name()?> wrote:</em>
+					<p><?=$comment->text()?></p>
+					<a href="index.php?action=delete&id=<?=$comment->id()?>"> delete comment</a>
+				</li>
+			</ul>	
+		<? endforeach ?>
+		</div>
+		<div id="input">
+		   <form action="" method="post">
+		        <input type="hidden" name="submit_comment" value="1"/>
+		        <input type="text" size="20" id="shout_box_nick" name="nick" value="input name" onclick="this.value=''" /><br />
+		        <textarea id="shout_box_text" name="comment_text" value="input comment"></textarea><br />
+		        <input type="submit" id="shout_box_submit" name="submit_text" value="Submit"/>
+		   </form>
+		</div>
+	</div>
+</div>
