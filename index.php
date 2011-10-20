42f2a00ea852d3f1e04702043f6f313b2b32baaa
Šimun Strukan <simun.strukan@gmail.com>
2011-10-20 17:00:32 +0200

    Revert "Revert 6b7d104c2421e4abe6345864c67cd132ec38f060^..HEAD"
    This reverts commit 60bf811e7b152e003c4f2b012200bf255654bacc.

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php 
-   
-    # Connect to MySQL database
-    include_once "class_lib.php";
-    $db = new Database('localhost', 'root', 'root', 'development');
-    $db->connect();
-	ob_start();
-    if(isset($_GET['action'])) { $action = $_GET['action']; }
-    if(isset($_GET['id']))     { $id = $_GET['id']; }
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
